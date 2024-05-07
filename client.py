c17f3a46ce706c70c22e32890e8a6460382c840f
Ferjodios <100472111@alumnos.uc3m.es>
2024-05-07 18:40:51 +0200

    pruenbiña mala

--- client.py
+++ client.py
@@ -37,7 +37,6 @@ class client :
         response = ''
         while True:
             server_response = socket.recv(1)
-            print(server_response)
             if (server_response == b'\0'):
                 break
             response += server_response.decode()
@@ -392,11 +391,15 @@ class client :
                 client._user.encode('utf-8') + b'\0'
             )
             socketS.sendall(message)
-            response = client.read_response(socketS)
+            response = socket.recv(1)
+            # response = client.read_response(socketS)
             #Gestion del resultado
             if response == '0':
+                
+                response = socket.recv(1)
+                number_of_userss = socket.recv(1)
                 # Recibir toda la lista de usuarios
-                number_of_userss = client.read_response(socketS)
+                # number_of_userss = client.read_response(socketS)
                 print(number_of_userss)
                 number_of_users = int(number_of_userss)
                 for i in range(number_of_users):
